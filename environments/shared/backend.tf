2215198dbeb2798e4286fd46537e177d77ea6236
nihal <nihal@gmail.com>
2026-09-22 12:52:16 +0530

    2

--- environments/shared/backend.tf
+++ environments/shared/backend.tf
@@ -1,11 +1,11 @@
 
 terraform {
   backend "s3" {
-    bucket = "my-nihal-tfstate-files"
-    key = "shared-service/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "my-nihal-tfstate-files"
+    key            = "shared-service/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "TF-STATE"
-    encrypt = true
+    encrypt        = true
 
 
 
--- environments/shared/main.tf
+++ environments/shared/main.tf
@@ -2,9 +2,9 @@ module "vpc" {
 
   source = "../../module/vpc"
 
-  vpc_name           = var.vpc_name
-  vpc_cidr           = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
+  vpc_name            = var.vpc_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  az                 = var.availability_zone
+  az                  = var.availability_zone
 }
